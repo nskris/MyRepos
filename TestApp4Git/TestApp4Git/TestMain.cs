58a3972277fae88edfb1c2a47b5f147c66d4bfb2
nskris <nskris@gmail.com>
2018-06-04 17:31:26 -0300

    iaaa

---
+++ TestApp4Git/TestApp4Git/TestMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp4Git
+{
+    public class TestMain
+    {
+
+        public void Func()
+        {
+            try
+            {
+                using (StreamWriter outputFile = new StreamWriter("C:\\test\\WriteLines.txt"))
+                {
+                    outputFile.WriteLine("Here is the first stringA.");
+                };
+
+            }
+            catch (Exception ex)
+            {
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                    eventLog.WriteEntry(ex.Message, EventLogEntryType.Information, 101, 1);
+                }
+            }
+        }
+    }
+}
