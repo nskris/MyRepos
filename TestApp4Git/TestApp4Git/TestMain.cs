35eba3ca8547158578e001ea07a3b0158e3cb330
nskris <nskris@gmail.com>
2018-06-11 11:55:55 -0300

    Message

--- TestApp4Git/TestApp4Git/TestMain.cs
+++ TestApp4Git/TestApp4Git/TestMain.cs
@@ -17,7 +17,7 @@ namespace TestApp4Git
             {
                 using (StreamWriter outputFile = new StreamWriter("C:\\test\\WriteLines.txt"))
                 {
-                    outputFile.WriteLine("Here is the first stringM.");
+                    outputFile.WriteLine("Here is the first stringN.");
                 };
 
             }
